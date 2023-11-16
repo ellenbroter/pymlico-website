3611356a91b681283da62373f6f8905fd62146bb
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2023-11-16 15:54:02 +0100

    Filter js and css

---
+++ js/concert-history.js
@@ -0,0 +1,25 @@
+const filterButtons = document.querySelectorAll('.filter-button');
+const listItems = document.querySelectorAll('.list-item'); //NodeList
+const listItemsContainer = document.querySelector('.list-items');
+
+filterButtons.forEach(filterButton => {
+	const filterList = (event) => {
+		const currentButton = event.currentTarget;
+		const currentButtonFilterBy = currentButton.dataset.filterBy;
+		const filteredItems = [...listItems].filter(item => {
+			if (currentButtonFilterBy === '*') {
+				return true;
+			} else {
+				return item.dataset.type === currentButtonFilterBy;
+			}
+		});
+
+		listItemsContainer.innerHTML = '';
+		
+		filteredItems.forEach(item => {
+			listItemsContainer.appendChild(item);
+		});
+	}
+
+	filterButton.addEventListener('click', filterList);
+});
